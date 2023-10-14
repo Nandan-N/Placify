435dfcfa9f92b4d651053d24ddcf1b5763ace299
Shreeja R <92515472+Mystery510@users.noreply.github.com>
2023-10-14 20:37:32 +0530

    Add files via upload

---
+++ HomeJobs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Job from './pages/Job'
+import NavBar1 from './pages/NavBar1'
+
+function Homejobs() {
+  return (
+    <div className='homejob'>
+  
+      <NavBar1/>
+      <Job />
+    </div>
+  );
+}
+
+export default Homejobs;
